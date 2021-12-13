ea827671ec4cf5e6ce80a24fec28b25da3e537bf
xpusz3k <94309279+xpusz3k@users.noreply.github.com>
2021-12-13 17:46:00 +0100

    Update index.js
    Xz

--- strony/index.js
+++ strony/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>HELLO</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         <h1>witaj!</h1>
         </p>
       </main>
       <a href="hosting">pozdro</a>
